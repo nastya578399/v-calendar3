c14c745a962469d16cc59eb6d0e6c53c0a18216e
Romanova Nastya <opt57839@mail.ru>
2021-10-11 20:10:20 +0300

    first commit

---
+++ src/components/Calendar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <v-date-picker v-model="date"
+        is-dark
+        is-expanded
+        title-position="left"
+        :masks="{ title: 'MMMM YYYY' }"
+        >
+        </v-date-picker>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+
+}
+</script>
+
+<style>
+.vc-container.vc-is-dark{
+    color: white;
+    background-color: #3d3d3e;
+}
+
+</style>
